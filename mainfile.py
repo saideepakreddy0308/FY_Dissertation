e09c1562216f65f320e0bee5f12ab4eca1107696
MOON KNIGHT <90776053+saideepakreddy0308@users.noreply.github.com>
2023-09-14 11:04:16 +0530

    5 years etf data download

--- mainfile.py
+++ mainfile.py
@@ -1,16 +1,44 @@
+import os
+import schedule
+import time
 import yfinance as yf
+import pandas as pd
 
-# For Equity-based ETF (e.g., SPY)
-equity_etf = yf.download('SPY', start='2010-01-01', end='2021-09-01')
+# Define list of ETF tickers
+etf_list = ["SPY", "QQQ", "EEM", "GLD", "SLV"]
 
-# Displaying the first few rows
-print(equity_etf.head())
+# Define date range (for this example, past 5 years)
+start_date = "2016-01-0"
+end_date = "2021-01-01"
 
-# For Daily Timeframe
-# daily_data = yf.download('SPY', start='2010-01-01', end='2021-09-01', interval='1d')
+# Create directory to store data if it doesn't exist
+if not os.path.exists("etf_data"):
+    os.makedirs("etf_data")
 
-# For Monthly Timeframe
-# monthly_data = yf.download('SPY', start='2010-01-01', end='2021-09-01', interval='1mo')
+def download_data():
+    for ticker in etf_list:
+        # Fetch data from Yahoo Finance
+        data = yf.download(ticker, start=start_date, end=end_date)
 
-# For Quarterly Timeframe
-# quarterly_data = yf.download('SPY', start='2010-01-01', end='2021-09-01', interval='3mo')
+        # Data integrity check for missing values
+        if data.isnull().values.any():
+            print(f"Warning: Missing values found in data for {ticker}")
+
+        # Save data to CSV
+        data.to_csv(f"etf_data/{ticker}_{start_date}_to_{end_date}.csv")
+
+        print(f"Data for {ticker} saved.")
+
+# Function to run the job every day
+def job():
+    print("Downloading ETF data...")
+    download_data()
+    print("Download complete.")
+
+# Schedule the job to run once every day
+schedule.every().day.at("09:00").do(job)
+
+# Keep the script running
+while True:
+    schedule.run_pending()
+    time.sleep(1)
